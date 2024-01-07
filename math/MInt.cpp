7becfacbfbce6952581819f9b6335d06acb9831c
Nanaanaaa <3516892226@qq.com>
2024-01-07 15:46:10 +0800

    222

--- math/MInt.cpp
+++ math/MInt.cpp
@@ -2,12 +2,17 @@ constexpr int P = 998244353;
 template<class T>
 constexpr T power(T a, i64 b) {
     T res = 1;
+    int sgn = 1;
+    if (b < 0) {
+        sgn *= -1;
+        b *= -1;
+    }
     for (; b; b /= 2, a *= a) {
         if (b % 2) {
             res *= a;
         }
     }
-    return res;
+    return sgn == -1 ? 1 / res : res;
 }
 template<int P = ::P>
 struct MInt {
--- math/power.cpp
+++ math/power.cpp
@@ -1,9 +1,15 @@
-constexpr i64 power(i64 a, i64 b, i64 P = ::P) {
-    i64 res = 1;
-    for (; b; b /= 2, a = a * a % P) {
+template<class T>
+constexpr T power(T a, i64 b) {
+    T res = 1;
+    int sgn = 1;
+    if (b < 0) {
+        sgn *= -1;
+        b *= -1;
+    }
+    for (; b; b /= 2, a *= a) {
         if (b % 2) {
-            res = res * a % P;
+            res *= a;
         }
     }
-    return res;
+    return sgn == -1 ? 1 / res : res;
 }
