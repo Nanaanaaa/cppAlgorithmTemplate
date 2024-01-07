ccdc22ffecca2463c109a2db73a5dd108b34e6a4
Nanaanaaa <3516892226@qq.com>
2024-01-07 11:25:11 +0800

    23232

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,35 +1,10 @@
-// 本质不同的边多 用priority_queue 否则std::set
-template<class T = int>
-std::vector<T> dijkstra(int s, auto& adj) {
-    using e = std::pair<T, int>;
-    const int n = adj.size();
-
-    std::vector<T> dis(n, -1);
-    std::set<e> S;
-    S.emplace(0, s);
-
-    while (!S.empty()) {
-        auto [d, u] = *S.begin();
-        S.erase(S.begin());
-
-        if (dis[u] != -1) continue;
-        dis[u] = d;
-
-        for (auto [v, w] : adj[u]) {
-            S.emplace(d + w, v);
-        }
-    }
-    return dis;
-}
-
 // std::priority_queue
-
 template<class T = int>
-std::vector<T> dijkstra(int s, auto& adj) {
+std::vector<T> dijkstra(int s, const auto& adj, T init_ = -1) {
     using e = std::pair<T, int>;
     const int n = adj.size();
 
-    std::vector<T> dis(n, -1);
+    std::vector<T> dis(n, init_);
     std::priority_queue<e, std::vector<e>, std::greater<>> S;
     S.emplace(0, s);
 
@@ -37,46 +12,12 @@ std::vector<T> dijkstra(int s, auto& adj) {
         auto [d, u] = S.top();
         S.pop();
 
-        if (dis[u] != -1) continue;
+        if (dis[u] != init_) continue;
         dis[u] = d;
 
         for (auto [v, w] : adj[u]) {
             S.emplace(d + w, v);
         }
-    }
-    return dis;
-}
-
-// 朴素 小常数
-template<class T = int>
-std::vector<T> dijkstra(int s, auto& adj) {
-    using e = std::pair<T, int>;
-    const T inf = std::numeric_limits<T>::max();
-    const int n = adj.size();
-
-    std::vector<T> dis(n, inf);
-    std::vector<bool> vis(n);
-    std::set<e> S;
-
-    S.emplace(0, s);
-    dis[s] = 0;
-    while (!S.empty()) {
-        auto [d, u] = *S.begin();
-        S.erase(S.begin());
-
-        if (vis[u]) continue;
-        vis[u] = true;
-
-        for (auto [v, w] : adj[u]) {
-            if (dis[v] > d + w) {
-                dis[v] = d + w;
-                S.emplace(d + w, v);
-            }
-        }
-    }
-
-    for (auto& x : dis) {
-        if (x == inf) x = -1;
     }
     return dis;
 }
