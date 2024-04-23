7dc8a293508c6ddd0a8f167cab421132e5fbceec
Nanaanaaa <3516892226@qq.com>
2024-04-23 21:54:51 +0800

    12

--- DataStruct/StringHash.cpp
+++ DataStruct/StringHash.cpp
@@ -1,14 +1,33 @@
 std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
 
+constexpr bool isprime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+constexpr int findPrime(int n) {
+    while (!isprime(n)) {
+        n++;
+    }
+    return n;
+}
+
 struct StringHash {
-    const int P = findPrime(rnd() % 900000000 + 100000000);
+    const static int P;
     int n;
     std::vector<int> h, p, r;
     StringHash() {}
-    StringHash(std::string_view s) {
+    constexpr StringHash(std::string_view s) {
         init(s);
     }
-    void init(std::string_view s) {
+    constexpr void init(std::string_view s) {
         n = s.size();
         p.assign(n + 1, 1);
         h.assign(n + 1, 0);
@@ -20,32 +39,14 @@ struct StringHash {
         }
     }
 
-    bool isprime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    int findPrime(int n) {
-        while (!isprime(n)) {
-            n++;
-        }
-        return n;
-    }
-
-    int get(int l, int r) { // 0开头 左闭右开
+    constexpr int get(int l, int r) {
         return (h[r] + 1LL * (P - h[l]) * p[r - l]) % P;
     }
-    bool same(int x, int y, int l, int r) {
+    constexpr bool same(int x, int y, int l, int r) {
         return get(x, y) == get(l, r);
     }
-    bool isPalindrom(int x, int y) {
+    constexpr bool isPalindrom(int x, int y) {
         return (r[n - x] + 1LL * (P - r[n - y]) * p[y - x]) % P == get(x, y);
     }
 };
+const int StringHash::P = findPrime(rnd() % 900000000 + 100000000);
