892dfc2ae2f41e7b908dd8e3595095330d1a9421
Nanaanaaa <3516892226@qq.com>
2024-10-06 23:26:49 +0800

    11

--- DataStruct/ChthollyTree.cpp
+++ DataStruct/ChthollyTree.cpp
@@ -1,26 +1,20 @@
-struct ChthollyTree {
-    std::map<int, i64> map;
-
-    ChthollyTree() {}
-    constexpr i64& operator[](int x) {
-        return map[x];
-    }
-
+template<class K, class V>
+struct ChthollyTree : std::map<K, V> {
     void split(int p) {
-        map[p] = std::prev(map.upper_bound(p))->second;
+        this->emplace(p, std::prev(this->upper_bound(p))->second);
     }
 
     void rangeAssign(int l, int r, int v) {
         split(l);
         split(r);
-        for (auto it = map.find(l); it->first < r; it = map.erase(it)) {}
-        map[l] = v;
+        for (auto it = this->find(l); it->first < r; it = this->erase(it)) {}
+        this->emplace(l, v);
     }
 
     void rangeAdd(int l, int r, int v) {
         split(l);
         split(r);
-        for (auto it = map.find(l); it->first < r; it++) {
+        for (auto it = this->find(l); it->first < r; it++) {
             it->second += v;
         }
     }
@@ -28,11 +22,11 @@ struct ChthollyTree {
         split(l);
         split(r);
         std::vector<std::pair<i64, int>> t;
-        for (auto it = map.find(l); it->first < r; it++) {
+        for (auto it = this->find(l); it->first < r; it++) {
             t.emplace_back(it->second, std::next(it)->first - it->first);
         }
         std::sort(t.begin(), t.end());
-        for (auto&& [x, siz] : t) {
+        for (auto [x, siz] : t) {
             k -= siz;
             if (k <= 0) {
                 return x;
@@ -45,7 +39,7 @@ struct ChthollyTree {
         i64 ans = 0;
         split(l);
         split(r);
-        for (auto it = map.find(l); it->first < r; it++) {
+        for (auto it = this->find(l); it->first < r; it++) {
             ans += power(it->second, x, y) * (std::next(it)->first - it->first) % y;
             ans %= y;
         }
