8a470c8ac36aa115933c0332ae7079fa2d665e57
Nanaanaaa <3516892226@qq.com>
2024-10-07 00:51:57 +0800

    Trie

--- DataStruct/Trie.cpp
+++ DataStruct/Trie.cpp
@@ -1,40 +1,54 @@
-struct Trie : std::vector<std::array<int, 2>> {
+template<class Info>
+struct Trie : std::vector<Info> {
     Trie(int n = 0) { this->reserve(n + 1); newNode(); }
 
     int newNode() {
-        this->push_back({});
+        this->emplace_back();
         return this->size() - 1;
     }
 
-    template<class T>
-    void add(T x) {
+    void add(std::string_view s) {
         int p = 0;
-        int hi = std::numeric_limits<T>::digits - 1;
-
-        for (int i = hi; i >= 0; i--) {
-            int d = x >> i & 1;
-            if (!(*this)[p][d]) {
-                (*this)[p][d] = newNode();
+        for (auto c : s) {
+            c -= 'a';
+            if (!(*this)[p][c]) {
+                (*this)[p][c] = newNode();
             }
-            p = (*this)[p][d];
+            p = (*this)[p][c];
+            this->at(p).siz++;
         }
+        this->at(p).cnt++;
     }
 
-    template<class T>
-    int query(T x) {
-        int p = 0;
-        T ans = 0;
-        int hi = std::numeric_limits<T>::digits - 1;
+    void add(const char* s) {
+        add(std::string_view(s));
+    }
 
-        for (int i = hi; i >= 0; i--) {
-            int d = x >> i & 1;
-            if ((*this)[p][!d]) {
-                ans |= 1LL << i;
-                p = (*this)[p][!d];
-            } else {
-                p = (*this)[p][d];
+    int query(std::string_view s) {
+        int p = 0;
+        for (auto c : s) {
+            c -= 'a';
+            p = (*this)[p][c];
+            if (p == 0) {
+                return 0;
             }
         }
-        return ans;
+        return this->at(p).cnt;
+    }
+
+    int query(const char* s) {
+        return query(std::string_view(s));
+    }
+};
+
+struct Info {
+    constexpr static int ALPHABET_SIZE = 26;
+
+    int cnt = 0;
+    int siz = 0;
+    std::array<int, ALPHABET_SIZE> tr{};
+
+    constexpr int& operator[](int i) {
+        return tr.at(i);
     }
 };
